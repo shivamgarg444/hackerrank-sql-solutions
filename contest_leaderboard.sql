f1a5176f352aa53f305869e0e07fe4b1c6b455a7
shivamgarg444 <85186816+shivamgarg444@users.noreply.github.com>
2021-06-14 22:37:07 +0530

    Update contest_leaderboard.sql

--- contest_leaderboard.sql
+++ contest_leaderboard.sql
@@ -3,12 +3,12 @@
 SELECT 
   h.hacker_id,
   h.name,
-  SUM(score) AS total_score
+  SUM(score) AS total_score   # score field used from subquery
 FROM Hackers h
-JOIN
+JOIN  # joining a regular table and a temp table 
      (SELECT hacker_id, MAX(score) AS score FROM Submissions GROUP BY challenge_id, hacker_id) AS maxs
 
 ON h.hacker_id = maxs.hacker_id
 GROUP BY h.hacker_id, h.name
-HAVING total_score > 0
+HAVING total_score > 0   # used in place of WHERE as aggregate functions andgroup by is used
 ORDER BY total_score DESC, h.hacker_id
